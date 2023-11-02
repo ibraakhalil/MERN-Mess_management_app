3c2ea299477f808b1f680596837cc8a03ee38fb9
Ibrahim <ibkhalil5425@gmail.com>
2023-11-02 11:04:17 +0600

    add db url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const DB_URL = process.env.DB_URL
+const DB_URL = process.env.DB_URL_2
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_URL, () => {
