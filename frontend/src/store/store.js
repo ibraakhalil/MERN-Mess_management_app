f106cae18b37877f73cdd02a67a7a79e4aa9d11d
ibrahim khalil <ibkhalil5425@gmail.com>
2024-01-14 21:13:53 +0600

    modified store.js devtools setting line

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,10 @@
-import {createStore, compose, applyMiddleware} from 'redux'
+import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducer/rootReducer'
 import thunk from 'redux-thunk'
 
-
 const store = createStore(rootReducer, compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 ))
-
 
 export default store
